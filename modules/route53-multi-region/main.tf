384516ea111e35c6825d7749650a780a5fd818c8
Yonathan Koren <yonkornilov@live.com>
2021-01-21 08:40:58 -0500

    Create and associate health checks with weighted regional records, in order to implement multi region failover.

--- modules/route53-multi-region/main.tf
+++ modules/route53-multi-region/main.tf
@@ -2,6 +2,32 @@ data "aws_route53_zone" "hosted_zone" {
   zone_id = var.route53_hosted_zone_id
 }
 
+resource "aws_route53_health_check" "api-a" {
+  fqdn              = var.api_endpoint_a
+  port              = 443
+  type              = "HTTPS"
+  resource_path     = "/"
+  failure_threshold = "2"
+  request_interval  = "30"
+
+  tags = {
+    Name = "api-a-health-check"
+  }
+}
+
+resource "aws_route53_health_check" "api-b" {
+  fqdn              = var.api_endpoint_b
+  port              = 443
+  type              = "HTTPS"
+  resource_path     = "/"
+  failure_threshold = "2"
+  request_interval  = "30"
+
+  tags = {
+    Name = "api-b-health-check"
+  }
+}
+
 resource "aws_route53_record" "api-a" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = var.route53_api_subdomain
@@ -12,8 +38,9 @@ resource "aws_route53_record" "api-a" {
     weight = 50
   }
 
-  set_identifier = "api-a"
-  records        = [var.api_endpoint_a]
+  health_check_id = aws_route53_health_check.api-a.id
+  set_identifier  = "api-a"
+  records         = [var.api_endpoint_a]
 }
 
 resource "aws_route53_record" "api-b" {
@@ -26,6 +53,7 @@ resource "aws_route53_record" "api-b" {
     weight = 50
   }
 
+  health_check_id = aws_route53_health_check.api-b.id
   set_identifier = "api-b"
   records        = [var.api_endpoint_b]
 }
