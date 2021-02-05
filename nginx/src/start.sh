f7c203927377d0e15de5724a752fc01f2520d3ce
Yonathan Koren <yonkornilov@live.com>
2021-02-05 12:49:23 -0500

    Render configuration visibly inside start.sh script.

--- nginx/src/start.sh
+++ nginx/src/start.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-/usr/bin/envsubst < /etc/nginx/nginx.conf.envsubst > /etc/nginx/nginx.conf
+config_template_file="/etc/nginx/nginx.conf.envsubst"
+config_file="/etc/nginx/nginx.conf"
+
+cat << EOF
+Generating configuration in ${config_file}
+---
+$(/usr/bin/envsubst < ${config_template_file} | tee ${config_file})
+---
+EOF
+
 exec nginx -g "daemon off;";
